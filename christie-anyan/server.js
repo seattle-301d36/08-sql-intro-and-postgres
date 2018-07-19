71a1f9c9d2c51020f7d9b6f580bf4453a530298d
tganyan <tganyan@gmail.com>
2018-07-19 12:09:29 -0700

    added sql query to get articles to render in the view

--- christie-anyan/server.js
+++ christie-anyan/server.js
@@ -37,7 +37,7 @@ app.get('/new-article', (request, response) => {
 app.get('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // PUT YOUR RESPONSE HERE
-  client.query('')
+  client.query('SELECT * FROM articles;')
     .then(function(result) {
       response.send(result.rows);
     })
@@ -119,7 +119,7 @@ app.delete('/articles', (request, response) => {
 });
 
 // COMMENT: What is this function invocation doing?
-// PUT YOUR RESPONSE HERE
+// Creating the DB if it doesn't exist and running loadArticles().
 loadDB();
 
 app.listen(PORT, () => {
@@ -131,7 +131,7 @@ app.listen(PORT, () => {
 ////////////////////////////////////////
 function loadArticles() {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // 
 
   let SQL = 'SELECT COUNT(*) FROM articles';
   client.query( SQL )
